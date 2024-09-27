a49c83f3004e1882f1c4c434376ff592f3a2261a
prefer_choi <prefer_choi@naver.com>
2024-09-27 09:35:00 +0900

    HashRouter로 변경 PostDetails, NotFound 추가

--- iam/src/App.tsx
+++ iam/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Projects from "./pages/Projects";
@@ -8,44 +8,60 @@ import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
 import Resume from "./pages/Resume";
+import PostDetails from "./pages/PostDetails";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App bg-gray-100 text-gray-900">
       <nav className="w-full p-4 bg-white shadow-md fixed top-0">
         <div className="container mx-auto flex justify-between items-center">
-          <a href="#" className="text-2xl font-bold">
+          <a href="/" className="text-2xl font-bold">
             최선호
           </a>
           <div className="space-x-4">
-            <a href="#about" className="hover:text-blue-500">
+            <a href="/#about_me" className="hover:text-blue-500">
               About
             </a>
-            <a href="#projects" className="hover:text-blue-500">
+            <a href="/#projects" className="hover:text-blue-500">
               Projects
             </a>
-            <a href="#skills" className="hover:text-blue-500">
+            <a href="/#skills" className="hover:text-blue-500">
               Skills
             </a>
-            <a href="#contact" className="hover:text-blue-500">
+            <a href="/#experience" className="hover:text-blue-500">
+              experience
+            </a>
+            <a href="/#contact" className="hover:text-blue-500">
               Contact
+            </a>
+            <a href="/#blog" className="hover:text-blue-500">
+              blog
+            </a>
+            <a href="/#resume" className="hover:text-blue-500">
+              resume
             </a>
           </div>
         </div>
       </nav>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/AboutMe" element={<AboutMe />} />
-          <Route path="/Projects" element={<Projects />} />
-          <Route path="/ProjectDetails" element={<ProjectDetails />} />
-          <Route path="/Skills" element={<Skills />} />
-          <Route path="/Experience" element={<Experience />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Blog" element={<Blog />} />
-          <Route path="/Resume" element={<Resume />} />
-        </Routes>
-      </BrowserRouter>
+      <main className="pt-16">
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about_me" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/project/:id" element={<ProjectDetails />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<PostDetails />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="*" element={<NotFound />} />{" "}
+            {/* Catch-all Route for 404 */}
+          </Routes>
+        </HashRouter>
+      </main>
     </div>
   );
 }
